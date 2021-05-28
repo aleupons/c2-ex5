d152ba1ed23619b6430fc96d06642a7789b0beca
aleupons <aleu.pons.alba@gmail.com>
2021-05-28 19:17:03 +0200

    Exercici 5

---
+++ js/index.js
@@ -0,0 +1,14 @@
+function esPalindromo(frase) {
+  const fraseModificada = frase
+    .toLowerCase()
+    .replaceAll(/[.,·\/#¡!$%\^&\*;:{}=\-_`~()”“"'‘’«»…?¿[\]\s]/g, "")
+    .normalize("NFD")
+    .replaceAll(/[\u0300-\u036f]/g, "");
+  const fraseDelReves = fraseModificada.split("").reverse().join("");
+  return fraseModificada === fraseDelReves;
+}
+
+const frase = "Höla, què tál?";
+const frase2 = "Argentina, la lluna anul·la la nit negra";
+console.log(esPalindromo(frase));
+console.log(esPalindromo(frase2));
